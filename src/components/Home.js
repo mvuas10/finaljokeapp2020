3816f8c876a05843a4138ec2a440188bd0d3484c
Minh Vu <mvuas10@Minhs-MacBook-Air.local>
2020-12-13 12:32:13 +0100

    Happy Logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Logo from "../components/images/Logo.png";
 import Button from "./Button";
+import NewLogo from "../components/images/NewLogo.png";
 
 export default function Home() {
   return (
     <div className="section">
       <div className="component-section-header">
-        <img src={Logo} alt="Furry animal" />
+        {/* <img src={Logo} alt="Furry animal" /> */}
+        <img src={NewLogo} alt="Christmas" />
 
         <h2>
           <div>Loving 2020 so far?</div>
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "../components/images/Logo.png";
+import NewLogo from "../components/images/NewLogo.png";
 
 export default function Toolbar() {
   return (
     <header id="header" className="section-header scroll">
       <a href="./">
-        <img className="icon" src={Logo} alt="Furry animal" />
+        {/* <img className="icon" src={Logo} alt="Furry animal" /> */}
+        <img className="icon" src={NewLogo} alt="Christmas" />
       </a>
 
       <nav>
